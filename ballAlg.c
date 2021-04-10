92f23fdaeca42f17b9ba22ea6b54239c83ccdfaf
Guilherme Mascarenhas <guifrei@live.com.pt>
2021-04-10 18:17:32 +0100

    Print to file

--- ballAlg.c
+++ ballAlg.c
@@ -37,9 +37,29 @@ node *newNode(double **arr, long n_points, int n_dims, int id)
     return aux;
 }
 
+void dump_tree(node *root){
+    if(root->left != NULL)
+        dump_tree(root->left);
+
+    if(root->right != NULL)
+        dump_tree(root->right);
+/*
+    for(int i=0; i<root->n_points; i++){
+        free(root->pts[i]);
+    }
+    */
+    for (int i=0; i<root->n_points; i++){
+        free(root->pts[i]);
+    }
+    free(root->pts);
+    free(root->center);
+    free(root);
+    return;
+}
+
 double comp_dist(double *a, double *b, int *n_dims)
 {
-    double aux, sum = 0, two = 2;
+    double aux = 0, sum = 0, two = 2;
     for (int i = 0; i < *n_dims; i++)
     {
         aux = a[i] - b[i];
@@ -60,6 +80,10 @@ double calcRadius(double **pt_arr, int n_dims, int *indices, double *center)
         return dist_b;
 }
 
+/*
+Recebe o conjunto de pontos (vetor)
+Retorna os indices dos dois pontos mais afastados um do outro
+*/
 int *furthest(double **pt_arr, long n_points, int n_dims)
 {
     double *aux = pt_arr[0];
@@ -126,10 +150,9 @@ double **ort_proj(double **pt_arr, long n_points, int n_dims, int *indices)
             {
                 x[j] = pt_arr[i][j] - a[j];
                 y[j] = b[j] - a[j];
-                //printf("%d dims\n", n_dims);
+                
             }
-            //printf("%f %f \n", x[0], x[1]);
-            //printf("%f %f \n", y[0], y[1]);
+
             aux1 = inner_product(x, y, n_dims);
             aux2 = inner_product(y, y, n_dims);
             aux1 = aux1 / aux2;
@@ -153,17 +176,16 @@ double **ort_proj(double **pt_arr, long n_points, int n_dims, int *indices)
 
 double **create_array_pts(int n_dims, long np)
 {
-    double *_p_arr;
     double **p_arr;
-    _p_arr = (double *)malloc(n_dims * np * sizeof(double));
     p_arr = (double **)malloc(np * sizeof(double *));
-    if ((_p_arr == NULL) || (p_arr == NULL))
+    if (p_arr == NULL)
     {
         printf("Error allocating array of points, exiting.\n");
         exit(4);
     }
     for (int i = 0; i < np; i++)
-        p_arr[i] = &_p_arr[i * n_dims];
+        p_arr[i] = (double *)malloc(n_dims* sizeof(double));
+        
     return p_arr;
 }
 
@@ -198,11 +220,6 @@ double **get_points(int argc, char *argv[], int *n_dims, long *np)
             pt_arr[i][j] = RANGE * ((double)random()) / RAND_MAX;
     return pt_arr;
 }
-
-/*int cmp (const void * a, const void * b) {
-    printf("%f - %f = %f\n",*(double*)a,*(double*)b, *(double*)a - *(double*)b);
-   return ( *(double*)a - *(double*)b );
-}*/
 
 int cmp(const void *a, const void *b)
 {
@@ -239,14 +256,18 @@ double *center(double **orto_points, long n_points, int n_dims, int *indices)
 {
     double *center = (double *)malloc(sizeof(double) * n_dims);
     double *aux = (double *)malloc(sizeof(double) * n_points);
-    for (long j = 0; j < n_points; j++)
-    {
-        aux[j] = orto_points[j][0];
+    for (int i = 0; i < n_dims; i++){
+        for (long j = 0; j < n_points; j++)
+        {
+            aux[j] = orto_points[j][i];
+        }
+        if (orto_points[0][i] != orto_points[1][i])
+        {
+            qsort(aux, n_points, sizeof(aux[0]), cmp);
+            break;
+        }
     }
-    if (orto_points[0][0] != orto_points[1][0])
-    {
-        qsort(aux, n_points, sizeof(aux[0]), cmp);
-    }
+
     /*printf("Ordered:\n");
     for (long j = 0; j < n_points; j++)
     {
@@ -282,15 +303,6 @@ int build_tree(node *root, int id)
         //Create orthogonal projection
         double **orto_points;
         orto_points = ort_proj(root->pts, root->n_points, root->n_dims, indices);
-        /*if (root->n_points == 3)
-        {
-            for (int i = 0; i < root->n_points; i++)
-            {
-                for (int j = 0; j < root->n_dims; j++)
-                    printf("%f ", orto_points[i][j]);
-                printf("\n");
-            }
-        }*/
 
         // Calcualte center and divide (verificar numero de pontos)
         root->center = center(orto_points, root->n_points, root->n_dims, indices);
@@ -300,7 +312,7 @@ int build_tree(node *root, int id)
         for (long i = 0; i < root->n_points; i++)
         {
             if (orto_points[i][0] < root->center[0])
-            {
+            {   
                 left[l_id] = root->pts[i];
                 l_id++;
             }
@@ -310,22 +322,10 @@ int build_tree(node *root, int id)
                 r_id++;
             }
         }
-        /*printf("Left\n");
-        for(long i=0; i<l_id;i++){
-            for (int j = 0; j < root->n_dims; j++)
-                printf("%f ", left[i][j]);
-            printf("\n");
-        }
-
-        printf("Right\n");
-        for(long i=0; i<r_id;i++){
-            for (int j = 0; j < root->n_dims; j++)
-                printf("%f ", right[i][j]);
-            printf("\n");
-        }*/
 
         // Print ID of node
         //print (radius & center)
+        /*
         printf("-------------------- \n");
         printf("Radius: %f \n", root->radius);
         printf("Center: ");
@@ -334,6 +334,7 @@ int build_tree(node *root, int id)
             printf("%f ", root->center[i]);
         }
         printf("\nNPoints: %d\n", root->n_points);
+        */
 
         // call left node
         int _id = id + 1;
@@ -355,6 +356,7 @@ int build_tree(node *root, int id)
         root->center = root->pts[0];
         // Print ID of node
         //print (radius & center)
+        /*
         printf("--------------------end \n");
         printf("Radius: %f \n", root->radius);
         printf("Center: ");
@@ -363,11 +365,13 @@ int build_tree(node *root, int id)
             printf("%f ", root->center[i]);
         }
         printf("\nNPoints: %d\n", root->n_points);
+        printf("Id: %d\n", id);
+        */
     }
     return id;
 }
 
-void printNode(node *root)
+void printNode(node *root, FILE* fp)
 {
     int id_left;
     int id_right;
@@ -381,23 +385,38 @@ void printNode(node *root)
     else
         id_right = root->right->id;
 
-    printf("%d %d %d %f ", root->id, id_left, id_right, root->radius);
+    if (root->left != NULL)
+        printNode(root->left, fp);
+    if (root->right != NULL)
+        printNode(root->right, fp);
+
+    ///////////////////////////////////////////////////////////////
+    fprintf(fp, "%d %d %d %f ", root->id, id_left, id_right, root->radius);
+    ////////////////////////////////////////////////////////////////
+    //printf("%d %d %d %f ", root->id, id_left, id_right, root->radius);
     for (int j = 0; j < root->n_dims; j++)
     {
-        printf("%f ", root->center[j]);
+        fprintf(fp, "%f ", root->center[j]);
     }
-    printf("\n");
-    if (root->left != NULL)
-        printNode(root->left);
-    if (root->right != NULL)
-        printNode(root->right);
+    fprintf(fp, "\n");
 }
 
-void printTree(node *root)
+void printTree(node *root, int max_id)
 {
-    int id = 0;
-    printf("%d %d\n", root->n_dims, root->n_points);
-    printNode(root);
+    //Os id's dos pontos começam em 0. Por isso o nº de nós é igual ao indice máximo + 1
+    int n_nodes = max_id + 1;
+
+    puts("Escrita de ficheiro");
+    ///////////////////////////////////////////////////////////////
+    FILE *fp = NULL;
+    fp = fopen("exemplo.txt", "a");
+    if(!fp) perror("fopen");
+    fprintf(fp, "%d %d\n", root->n_dims, n_nodes);
+    ////////////////////////////////////////////////////////////////
+
+    //printf("%d %d\n", root->n_dims, n_nodes);
+    printNode(root, fp);
+    fclose(fp);
 }
 
 int main(int argc, char *argv[])
@@ -405,27 +424,33 @@ int main(int argc, char *argv[])
     int n_dims = 0;
     long n_points = 0;
     double **pts;
+    int max_id = 0;
 
     double exec_time;
     exec_time = -omp_get_wtime();
     pts = get_points(argc, argv, &n_dims, &n_points);
 
+/*
     for (int i = 0; i < n_points; i++)
     {
         for (int j = 0; j < n_dims; j++)
             printf("%f ", pts[i][j]);
         printf("\n");
     }
+*/
 
     //furthest(pts,n_points,n_dims);
 
     node *root = newNode(pts, n_points, n_dims, 0);
-    build_tree(root, 0);
+
     //build tree
+    max_id=build_tree(root, 0);
 
     exec_time += omp_get_wtime();
-    fprintf(stderr, "%.1lf\n", exec_time);
+    fprintf(stderr, "%.1lf s\n", exec_time);
+
+    printTree(root, max_id);
 
     //dump tree
-    printTree(root);
+    //dump_tree(root);
 }
